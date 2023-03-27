2225a88d47abf86cd580e5cdd2edb7d381bb6a65
Sarang <rajurkarsarang7@gmail.com>
2023-03-27 12:37:32 +0530

    adding files

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONG_URL, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
